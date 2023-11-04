6e1fa549e280938b32f2c6fa94764a6564600bfc
Muhammad Nadeem <70134301+imuhammadnadeem@users.noreply.github.com>
2023-11-04 19:35:10 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseCrashlytics } from '@capacitor-firebase/crashlytics';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    setTimeout(() => {
+      this.isCrashlyticsEnabled();
+    }, 10000);
+  }
+
+  async isCrashlyticsEnabled() {
+    const result = await FirebaseCrashlytics.isEnabled();
+    return result.enabled;
+  };
 }
